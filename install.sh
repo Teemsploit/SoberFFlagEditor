3dcab2f57b163e1b436330b42add9ee84bb0c8cd
Teemsploit <53855235+Teemsploit@users.noreply.github.com>
2025-06-11 09:56:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/Teemsploit/SoberFFlagEditor.git
+cd SoberFFlagEditor
 gcc $(pkg-config --cflags --libs json-c) -o fflag SoberFFlagEditor.c
 sudo cp fflag /usr/local/bin/
 sudo chmod +x /usr/local/bin/fflag
